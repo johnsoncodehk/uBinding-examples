f80beed33c4438249055e395bfadf917615f775d
johnsoncodehk <johnsoncodehk@gmail.com>
2019-05-11 09:43:24 +0800

    Add MVP Example

---
+++ Assets/4. MVP Example/MVP_Model.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using uBinding;
+
+public class MVP_Model : MonoBehaviour
+{
+    [Range(0, 1)] public BindableFloat volume;
+}
---
+++ Assets/4. MVP Example/MVP_Presenter.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MVP_Presenter : MonoBehaviour
+{
+
+    public MVP_Model model;
+    public MVP_View view;
+
+    void Start()
+    {
+        view.slider.onValueChanged.AddListener(vol =>
+        {
+            model.volume.value = vol;
+        });
+    }
+    void OnEnable()
+    {
+        model.volume += OnVolumeChanged;
+    }
+    void OnDisable()
+    {
+        model.volume -= OnVolumeChanged;
+    }
+
+    void OnVolumeChanged(float volum)
+    {
+        view.slider.value = volum;
+        view.text.text = "Volum: " + volum;
+    }
+}
